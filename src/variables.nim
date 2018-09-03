02a5c4f5aa75c709c63afad815529c7302eb2483
juancarlospaco <juancarlospaco@gmail.com>
2018-09-03 09:34:41 -0300

    Add DocStrings

--- src/variables.nim
+++ src/variables.nim
@@ -1,18 +1,18 @@
 let
   start_time = cpuTime()
-  plugins_folder = getCurrentDir() / "plugins"
-  bash_plugins_folder = plugins_folder / "bash"
-  python_plugins_folder = plugins_folder / "python"
-  static_plugins_folder = plugins_folder / "static"
-  geo_plugins_folder = plugins_folder / "geo"
-  config_ini = loadConfig("config.ini")
-  api_key    = config_ini.getSectionValue("", "api_key")
-  cli_colors = parseBool(config_ini.getSectionValue("", "terminal_colors"))
-  ips2ping = config_ini.getSectionValue("", "ips2ping").split(',')
+  plugins_folder* = getCurrentDir() / "plugins"      ## Plugins main folder path, that contains all other subfolder by plugin type.
+  bash_plugins_folder* = plugins_folder / "bash"     ## Bash Plugins folder path, that contains all ``*.sh`` Bash plugins.
+  python_plugins_folder* = plugins_folder / "python" ## Python Plugins folder path, that contains all ``*.py`` Python plugins.
+  static_plugins_folder* = plugins_folder / "static" ## Static files "Plugins" folder path, that contains all ``*.*`` plugins. Its similar to Djangos/Flask Static folder.
+  geo_plugins_folder* = plugins_folder / "geo"       ## INI Plugins folder path, that contains all ``*.ini`` Geo Location plugins.
+  config_ini* = loadConfig("config.ini")             ## Load the ``config.ini`` file to get all the configs for the bot.
+  api_key*    = config_ini.getSectionValue("", "api_key")  ## Telegram API Key, its like the ID of the Bot, its given by ``@BotFather``, not technically a password.
+  cli_colors* = parseBool(config_ini.getSectionValue("", "terminal_colors"))  ## Use Terminal Colors or not. It allows Color Blind people to use the bot comfortably.
+  ips2ping* = config_ini.getSectionValue("", "ips2ping").split(',')  ## List of IP to ping when the ``/ping`` command is used on the chat.
   # folders2backup = parseJson(config_ini.getSectionValue("", "folders2backup"))
 
-  file_size_limit = parseInt(config_ini.getSectionValue("nim_files_crosscompilation", "size_limit"))
-  file_lineno_limit = parseInt(config_ini.getSectionValue("nim_files_crosscompilation", "lines_limit"))
+  file_size_limit* = parseInt(config_ini.getSectionValue("nim_files_crosscompilation", "size_limit"))    ## File Size Limit for CrossCompilation on demand.
+  file_lineno_limit* = parseInt(config_ini.getSectionValue("nim_files_crosscompilation", "lines_limit")) ## File Line count Limit for CrossCompilation on demand.
   linux_args = config_ini.getSectionValue("nim_files_crosscompilation", "linux_args")
   windows_args = config_ini.getSectionValue("nim_files_crosscompilation", "windows_args")
 
